75b66edac913d0cdabba3f064c980fe3823c0067
Bhanu Murari Sankarasetty <168958411+BhanuSankarasetty@users.noreply.github.com>
2025-08-14 01:28:31 +0530

    changed hero section resposveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react' 
+import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Manager from './components/Manager'
 import Footer from './components/Footer'
 
-function App() { 
+function App() {
 
   return (
     <>
-        <Navbar /> 
-       <Manager/> 
+      <Navbar />
+      <div className="bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+        <h1 className="text-3xl text-center font-bold text-gray-700 py-4">Secure Box</h1>
+        <Manager />
+      </div>
 
-       <Footer/>
+      <Footer />
     </>
   )
 }

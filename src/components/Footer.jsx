4852e2dd8cc31540034ca4ac7cbcb87a463832d2
Bhanu Murari Sankarasetty <168958411+BhanuSankarasetty@users.noreply.github.com>
2025-08-13 23:46:52 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-800 text-white flex flex-col justify-center items-center h-[7vh] w-full'>
+            <div className="logo font-bold text-white text-xl">
+                <span>Secure Your data-Password Manager</span>
+            </div>
+            <div className='flex justify-center items-center'> </div>
+        </div>
+    )
+}
+
+export default Footer
